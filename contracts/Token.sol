64048a798b03f1254e44907aa1495445aacbc7a0
Bart Bouman <bartbouman22@gmail.com>
2021-05-24 20:56:32 +0200

    refactoring

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
 
-import "./libraries/Ownable.sol";
+import "./Ownable.sol";
 import "./libraries/SafeMath.sol";
 
 contract Token is Ownable {
